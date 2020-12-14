a867b8d4fd4d7548ac1099dd7eddf3bef39cff61
ANILVM <30655235+ANILVM@users.noreply.github.com>
2020-12-14 11:13:31 +0530

    updated

--- workspacecleaup.sh
+++ workspacecleaup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-used=$(df -Ph | grep '/dev/mapper/vg_2g-lv_dbs' | awk {'print $5'})
+used=$(df -Ph | grep '/dev/sda1' | awk {'print $5'})
 max=80%
 if [ ${used%?} -ge ${max%?} ]; then
-echo "The Mount Point "/DB" on $(hostname) has used $used at $(date)" | mail -s "Disk space alert on $(hostname): $used used" anilvm09@gmail.com
+echo "The Mount "/DB" on $(hostname) has used $used at $(date)" | mail -s "Disk space alert on $(hostname): $used used" anilvm09@gmail.com
 rm -rf /path/workspace/*
 docker image prune -a
 fi
